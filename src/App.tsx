a62d1da059daa2db5ef996a51e3988d732277ffc
Douglas Souza <lyricsan@hotmail.com>
2023-12-26 14:31:24 -0300

    choosing style dynamically

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,52 @@
+import { useState } from "react";
+import { ButtonSize, ButtonType } from "./theme";
 import Button from "./components/Button";
-import { ButtonType, ButtonSize } from "./theme";
 
 export default function App() {
+  const [selectedType, setSelectedType] = useState(ButtonType.primary);
+  const [selectedSize, setSelectedSize] = useState(ButtonSize.lg);
+
   return (
-    <div className="flex items-center justify-center h-screen">
-      <Button type={ButtonType.delete} size={ButtonSize.lg}>
+    <div className="container mx-auto w-full max-w-xl h-screen flex flex-col items-center justify-center px-5">
+      <div className="w-full max-w-2xl text-center flex flex-col gap-5 my-7">
+        <div className="flex items-center gap-3">
+          <p className="text-xl">Choose button type: </p>
+          {["primary", "secondary", "basic", "delete"].map((type) => {
+            return (
+              <button
+                key={type}
+                onClick={() => {
+                  setSelectedType(ButtonType[type]);
+                }}
+              >
+                {type}
+              </button>
+            );
+          })}
+        </div>
+        <div className="flex items-center gap-3">
+          <p className="text-xl">Choose button size: </p>
+          {["sm", "lg"].map((size) => {
+            return (
+              <button
+                key={size}
+                onClick={() => {
+                  setSelectedSize(ButtonSize[size]);
+                }}
+              >
+                {size}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+      <Button type={selectedType} size={selectedSize}>
         button
       </Button>
+      <div className="flex flex-col gap-4 mt-8">
+        <p>SelectedType: {selectedType}</p>
+        <p>SelectedSize: {selectedSize}</p>
+      </div>
     </div>
   );
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,11 +1,18 @@
-export const ButtonType = {
+type ButtonTypeProps = {
+  [key: string]: string;
+};
+type ButtonSizeProps = {
+  [key: string]: string;
+};
+
+export const ButtonType: ButtonTypeProps = {
   primary: "bg-blue-500 hover:bg-blue-700 text-white font-bold rounded",
-  secondary: "bg-blue-500 hover:bg-blue-700 text-white font-bold rounded",
-  basic: "bg-white hover:bg-gray-700 text-gray-700 font-bold rounded",
+  secondary: "bg-orange-500 hover:bg-blue-700 text-white font-bold rounded",
+  basic: "bg-green-500 hover:bg-green-400 text-white font-bold rounded",
   delete: "bg-red-300 hover:bg-red-500 text-white font-bold rounded",
 };
 
-export const ButtonSize = {
+export const ButtonSize: ButtonSizeProps = {
   sm: "py-2 px-4 text-xs",
   lg: "py-3 px-6 text-lg",
 };
